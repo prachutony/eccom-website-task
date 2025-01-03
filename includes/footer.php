2a562ec2efe25ec0ddb0d468494cdd41a2717bde
prachutony <131603023+prachutony@users.noreply.github.com>
2025-01-03 20:07:52 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;AlphaThreads | All Rights Reserved | Contact Us: +91 1029384756</b></span></div>
+</footer>
+    
